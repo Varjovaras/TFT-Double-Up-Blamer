a031b92c4e7fc96685068cc3d60d392d1e283a29
Kristjan Rajaniemi <kristjan.rajaniemi@gmail.com>
2025-04-14 11:50:31 +0300

    get puuid for /api/matches

--- src/server/api/tft/puuid.ts
+++ src/server/api/tft/puuid.ts
@@ -1,23 +1,22 @@
-type AccountV1Response = {
-    puuid: string;
-    gameName: string;
-    tagLine: string;
-};
+import type { AccountV1Response } from "~/utils/types";
 
-export default defineEventHandler(async (_event) => {
-    // console.log(event);
-    const tagLine = "6969";
-    const name = "rollingntrolling";
+const getPuuidByAccountV1 = async (
+    accountName?: string,
+    accountTagLine?: string,
+) => {
+    const name = accountName || "rollingntrolling";
+    const tagLine = accountTagLine || "6969";
+
     const res = await fetch(
         `https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${name}/${tagLine}?api_key=${process.env.DEVELOPMENT_API_KEY}`,
     );
+
     if (!res.ok) {
         throw new Error(`API request failed with status: ${res.status}`);
     }
 
     const accountData = (await res.json()) as AccountV1Response;
+    return accountData;
+};
 
-    return {
-        puuId: accountData.puuid,
-    };
-});
+export default getPuuidByAccountV1;
