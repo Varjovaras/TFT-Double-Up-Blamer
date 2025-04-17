c8a3e2e1f0b87c618cdd81e597d98654640d4fbc
Kristjan Rajaniemi <kristjan.rajaniemi@gmail.com>
2025-04-17 13:22:13 +0300

    loading spinner component

---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="flex justify-center my-8">
+        <div
+            class="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-500"
+        />
+    </div>
+</template>
+
+<script setup lang="ts"></script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -13,15 +13,8 @@
             class="mb-8"
             @submit="handleSearch"
         />
+        <LoadingSpinner v-if="loading || loadingMatches" />
 
-        <!-- Loading state -->
-        <div v-if="loading || loadingMatches" class="flex justify-center my-8">
-            <div
-                class="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-500"
-            />
-        </div>
-
-        <!-- Error message -->
         <div
             v-if="error"
             class="bg-red-900/50 border border-red-500 text-white p-4 rounded-md mb-6 w-full"
